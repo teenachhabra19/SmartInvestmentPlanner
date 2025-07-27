c1d4f53824b86a11d115286d7065f7b6b3585333
Teena Chhabra <teenachhabra19@gmail.com>
2025-07-27 16:30:26 +0530

    Updated model

--- src/main/java/com/example/Smart_Investment_Planner/model/User.java
+++ src/main/java/com/example/Smart_Investment_Planner/model/User.java
@@ -9,11 +9,16 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="users")
+@Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 }
